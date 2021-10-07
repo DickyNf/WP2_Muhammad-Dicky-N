fe023278969c4d814c641260b9a28e37e68866f4
DickyNf <dickynurfajri1@gmail.com>
2021-10-07 21:58:44 +0700

    latihan pertemuan 1

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Muhammad Dicky Nurfajri
+            saya tinggal di daerah cengkareng
+            hobi saya bermain Games";
+    }
+}
